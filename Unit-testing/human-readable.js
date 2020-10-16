2c5e92853ff7bd846509fb45a53382168330ebf7
Jheavi <javierlaso213@hotmail.com>
2020-10-16 14:57:51 +0200

    function human readable and test

--- Unit-testing/human-readable.js
+++ Unit-testing/human-readable.js
@@ -1,5 +1,17 @@
 function humanReadable(seconds) {
+    seconds = Math.min(seconds, 359999);
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    minutes -= hours * 60;
+    minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    hours = hours < 10 ? `0${hours}` : `${hours}`;
+    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    debugger;
 
+    return `${hours}:${minutes}:${seconds}`
 }
+
+humanReadable(4855);
 
 module.exports = humanReadable;
--- Unit-testing/human-readable.test.js
+++ Unit-testing/human-readable.test.js
@@ -28,6 +28,15 @@ describe('Human-readable function', () => {
         expect(response).toEqual('00:01:00');
     });
     
+    test('Should return 01:01:01', () => {
+        //assign
+        const number = 3661;
+        //add
+        const response = humanReadable(number);
+        //assert
+        expect(response).toEqual('01:01:01');
+    });
+    
     test('Should return 23:59:59', () => {
         //assign
         const number = 86399;
@@ -40,6 +49,16 @@ describe('Human-readable function', () => {
     test('Should return 99:59:59', () => {
         //assign
         const number = 359999;
+        //add
+        const response = humanReadable(number);
+        //assert
+        expect(response).toEqual('99:59:59');
+    });
+    
+    
+    test('Should return 99:59:59 if more than 359999 seconds', () => {
+        //assign
+        const number = 380000;
         //add
         const response = humanReadable(number);
         //assert
